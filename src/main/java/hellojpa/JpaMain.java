a3784351a4688c0d2ad5bd713a49df4918da52a7
최승훈 (AI Industry플랫폼개발TF) <seunghoon.choi@kt.com>
2022-09-06 11:37:49 +0900

    상속관계 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
 
@@ -19,35 +18,22 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            team.setName("TeamA");
-            em.persist(team);
 
-            Member member = new Member();
-            member.setUsername("member1");
-            em.persist(member);
+            Movie movie = new Movie();
+            movie.setDirector("DirectorA");
+            movie.setActor("ActorA");
+            movie.setName("NameA");
+            movie.setPrice(12345);
 
-            // 양방향 매핑의 경우 양쪽에 데이터를 넣어주는 것이 좋다.
-            // 연관관계 편의 메소드로 대체
-//            team.getMembers().add(member);
+            em.persist(movie);
 
-//            member.changeTeam(team);
-            team.addMember(member);
+            em.flush();
+            em.clear();
 
-//            em.flush();
-//            em.clear();
+            Item findMovie = em.find(Item.class, movie.getId());
+            System.out.println(findMovie.getName());
 
-            Team team1 = em.find(Team.class, team.getId()); // 1차 캐시
-            List<Member> members = team1.getMembers();
-
-            for(Member m : members) {
-                System.out.println("m=" +  m.getUsername());
-            }
-
-            // 트랜잭션 커밋(내부적으로 flush)
-            System.out.println("=====BEFORE COMMIT===");
             tx.commit();
-            System.out.println("=====AFTER COMMIT===");
         } catch (Exception e) {
             e.printStackTrace();
             tx.rollback();
