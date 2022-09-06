c6bf38e3eb83f54a5a6f6d118bdf587dd42ef9ac
최승훈 (AI Industry플랫폼개발TF) <seunghoon.choi@kt.com>
2022-09-06 09:39:35 +0900

    양방향 일대일 매핑

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -13,6 +13,10 @@ public class Member {
     @Column(name = "USERNAME")
     private String username;
 
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
+
     @ManyToOne
     @JoinColumn(name = "TEAM_ID")
     private Team team;
